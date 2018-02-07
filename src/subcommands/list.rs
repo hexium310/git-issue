4150577c3aeb6ee0d10ac4ea6687c83c96271c04
hexium310 <hexium310@gmail.com>
2018-02-08 04:16:15 +0900

    Clean "list"

---
+++ src/subcommands/list.rs
@@ -0,0 +1,18 @@
+extern crate reqwest;
+extern crate serde_json;
+
+use std::process::exit;
+
+pub fn list<S: Into<String>>(owner: S, repo: S, query_string: S) {
+    let mut response = reqwest::get(&format!("https://api.github.com/repos/{}/{}/issues{}", owner.into(), repo.into(), query_string.into())).unwrap();
+    if !response.status().is_success() {
+        println!("failed to get issues.");
+        exit(1);
+    }
+
+    let text = response.text().unwrap();
+    let json = serde_json::from_str::<serde_json::Value>(&text).unwrap();
+    for array in json.as_array().unwrap() {
+        println!("{}\t{} ({})", array["number"], array["title"].as_str().unwrap(), array["html_url"].as_str().unwrap());
+    }
+}
