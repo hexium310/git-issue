b09878de01f63ff549e93b8656860485494396d3
hexium310 <hexium310@gmail.com>
2018-02-08 04:43:58 +0900

    Add list test

--- src/subcommands/list.rs
+++ src/subcommands/list.rs
@@ -7,6 +7,11 @@ pub fn list<S: Into<String>>(owner: S, repo: S, query_string: S) {
     let mut response = reqwest::get(&format!("https://api.github.com/repos/{}/{}/issues{}", owner.into(), repo.into(), query_string.into())).unwrap();
     if !response.status().is_success() {
         println!("failed to get issues.");
+
+        if cfg!(test) {
+            panic!();
+        }
+
         exit(1);
     }
 
@@ -15,4 +20,22 @@ pub fn list<S: Into<String>>(owner: S, repo: S, query_string: S) {
     for array in json.as_array().unwrap() {
         println!("{}\t{} ({})", array["number"], array["title"].as_str().unwrap(), array["html_url"].as_str().unwrap());
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::list;
+
+    #[test]
+    fn test_list_success() {
+        list("hexium310", "git-issue", "");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_list_failure() {
+        list("hexium310", "none-of-git-issue", "");
+    }
+
 }
